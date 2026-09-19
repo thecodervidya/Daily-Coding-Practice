29f9249fdc76460d9609a119809572da40eceb45
thecodervidya <vidyashrinaikwadi1911@gmail.com>
2026-09-19 09:11:53 +0530

    compare two number in c day-3 logic building

---
+++ comparetwonum.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a,b;
+	printf("Enter the value of a =");
+	scanf("%d",&a);
+	printf("\n Enter the value of b=");
+	scanf("%d",&b);
+	
+	if(a>b)
+	{
+		printf("\n a is greater than b");
+		
+	}
+	else
+	{
+		printf("b is grater");
+	}
+	return 0;
+}
