946ecdad6eacf2db36ec4e13912af758d2c70516
thecodervidya <vidyashrinaikwadi1911@gmail.com>
2026-09-19 09:34:28 +0530

    print array ele using cpp DSA day-3

---
+++ printarray.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a[5],i;
+	cout<<"Enter the array element=";
+	for(i=0;i<5;i++)
+	{
+		cin>>a[i];
+	}
+	
+	cout<<"\n Array elements=";
+	for(i=0;i<5;i++)
+	{
+		cout<<a[i];
+	}
+	return 0;
+	
+}
